a6aa8f19c7764d4001a5e0727100dbb706d1adf5
David Tsai <tsaitingyudavid@gmail.com>
2023-12-08 18:25:34 +0800

    add JS (WIP)

---
+++ Hex-flnal-project/script/all.js
@@ -0,0 +1,44 @@
+// 前台 JS
+
+// 取得產品列表，載入頁面時直接執行
+// 取得產品列表 DOM
+const productWrap = document.querySelector('.productWrap');
+
+getProductList();
+
+function getProductList() {
+    axios.get(
+      "https://livejs-api.hexschool.io/api/livejs/v1/customer/davidtt/products"
+    )
+    .then(res => {
+        const productList = res.data.products;
+        productList.forEach(item => {
+            console.log(item)
+            productWrap.innerHTML += `<li class="productCard">
+            <h4 class="productType">新品</h4>
+            <img
+              src="${item.images}"
+              alt=""
+            />
+            <a href="#" class="addCardBtn">加入購物車</a>
+            <h3>${item.title}</h3>
+            <del class="originPrice">NT$${item.origin_price}</del>
+            <p class="nowPrice">NT$${item.price}</p>
+          </li>`;
+        })
+    })
+    .catch(err => {
+        alert(err)
+    })
+}
+// 取得購物車列表，載入頁面時直接執行
+
+// 加入購物車
+
+// 編輯購物車產品數量
+
+// 刪除所有購物車資料
+
+// 刪除特定購物車資料
+
+// 送出購買訂單
