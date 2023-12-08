8740ee89a26522a4b008440b5376bbe4a0a42a48
David Tsai <tsaitingyudavid@gmail.com>
2023-12-08 18:40:14 +0800

    add JS (WIP)

--- Hex-flnal-project/script/all.js
+++ Hex-flnal-project/script/all.js
@@ -2,19 +2,20 @@
 
 // 取得產品列表，載入頁面時直接執行
 // 取得產品列表 DOM
-const productWrap = document.querySelector('.productWrap');
+const productWrap = document.querySelector(".productWrap");
 
 getProductList();
 
 function getProductList() {
-    axios.get(
+  axios
+    .get(
       "https://livejs-api.hexschool.io/api/livejs/v1/customer/davidtt/products"
     )
-    .then(res => {
-        const productList = res.data.products;
-        productList.forEach(item => {
-            console.log(item)
-            productWrap.innerHTML += `<li class="productCard">
+    .then((res) => {
+      const productList = res.data.products;
+      productList.forEach((item) => {
+        console.log(item);
+        productWrap.innerHTML += `<li class="productCard">
             <h4 class="productType">新品</h4>
             <img
               src="${item.images}"
@@ -25,14 +26,23 @@ function getProductList() {
             <del class="originPrice">NT$${item.origin_price}</del>
             <p class="nowPrice">NT$${item.price}</p>
           </li>`;
-        })
+      });
     })
-    .catch(err => {
-        alert(err)
-    })
+    .catch((err) => {
+      alert(err);
+    });
 }
 // 取得購物車列表，載入頁面時直接執行
 
+const cardItem = document.querySelector(".card-item");
+
+function getCardItem() {
+  axios
+    .get("livejs-api.hexschool.io/api/livejs/v1/customer/davidtt/carts")
+    .then((res) => {
+      console.log(res);
+    });
+}
 // 加入購物車
 
 // 編輯購物車產品數量
