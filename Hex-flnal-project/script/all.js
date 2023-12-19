07c9c559873a35f8102e97633fb2ca70e35905d4
David Tsai <tsaitingyudavid@gmail.com>
2023-12-19 23:27:01 +0800

    Fixing getCardItem func (undone)

--- Hex-flnal-project/script/all.js
+++ Hex-flnal-project/script/all.js
@@ -21,7 +21,7 @@ function getProductList() {
               src="${item.images}"
               alt=""
             />
-            <a href="#" class="addCardBtn">加入購物車</a>
+            <a href="#" class="addCardBtn" data-id=${item.id}>加入購物車</a>
             <h3>${item.title}</h3>
             <del class="originPrice">NT$${item.origin_price}</del>
             <p class="nowPrice">NT$${item.price}</p>
@@ -33,18 +33,18 @@ function getProductList() {
       alert("取得產品列表失敗，請聯繫親切的工程師");
     });
 }
-// 取得購物車列表，載入頁面時直接執行
 
+// 取得購物車列表，載入頁面時直接執行
+getCardItem();
 const cardItemWrapper = document.querySelector(".card-item-wrapper");
 const shoppingCartTotal = document.querySelector(".shoppingCartTotal");
-getCardItem();
 
 function getCardItem() {
   axios
     .get(`${baseUrl}/api/livejs/v1/customer/${api_path}/carts`)
     .then((res) => {
       res.data.carts.forEach((item) => {
-        cardItemWrapper.innerHTML += `<tr class="card-item">
+        cardItemWrapper.innerHTML = `<tr class="card-item">
         <td>
           <div class="cardItem-title">
             <img src="${item.product.images}" alt="" />
@@ -57,17 +57,38 @@ function getCardItem() {
         <td class="discardBtn">
           <a href="#" class="material-icons"> clear </a>
         </td>
-      </tr>`;
+</tr>`;
       });
       shoppingCartTotal.innerText = `NT$${res.data.finalTotal.toLocaleString()}`;
+      addToCartBtnListener();
     })
     .catch((err) => {
-      console.log(err);
       alert("取得購物車列表失敗，請聯繫親切的工程師");
     });
 }
-// 加入購物車
 
+// 為按鈕加上監聽，取得購物車列表時就直接呼叫
+function addToCartBtnListener() {
+  const addToCartBtnArray = document.querySelectorAll(".addCardBtn");
+  addToCartBtnArray.forEach(function (item) {
+    item.addEventListener("click", addToCart);
+  });
+}
+// 加入購物車
+function addToCart(e) {
+  e.preventDefault();
+  cardItemWrapper.innerHTML = "";
+  axios
+    .post(`${baseUrl}/api/livejs/v1/customer/${api_path}/carts`, {
+      data: {
+        productId: `${e.target.dataset.id}`,
+        quantity: 1,
+      },
+    })
+    .then((res) => {
+      getCardItem();
+    });
+}
 // 編輯購物車產品數量
 
 // 刪除所有購物車資料
@@ -79,6 +100,7 @@ function discardAll(e) {
   axios
     .delete(`${baseUrl}/api/livejs/v1/customer/${api_path}/carts`)
     .then((res) => {
+      shoppingCartTotal.textContent = "NT$0";
       cardItemWrapper.innerHTML = "購物車內沒有商品";
     })
     .catch((err) => {
