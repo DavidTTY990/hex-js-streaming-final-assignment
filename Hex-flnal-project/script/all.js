061bf7785869378dab1d708e131bfae463d2c114
David Tsai <tsaitingyudavid@gmail.com>
2023-12-26 10:38:08 +0800

    working on shopping cart message

--- Hex-flnal-project/script/all.js
+++ Hex-flnal-project/script/all.js
@@ -9,7 +9,7 @@ init();
 
 function init() {
   getProductList();
-  // getCardItem();
+  getCardItem();
 }
 // 取得產品列表
 const productWrap = document.querySelector(".productWrap");
@@ -32,81 +32,15 @@ function getProductList() {
             <p class="nowPrice">NT$${item.price}</p>
           </li>`;
       });
+      addToCartBtnListener();
     })
     .catch((err) => {
       console.log(err);
       alert("取得產品列表失敗，請聯繫親切的工程師");
     });
 }
-// 取得購物車列表
-const cardItemWrapper = document.querySelector(".card-item-wrapper");
-const shoppingCartTable = document.querySelector(".shoppingCart-table");
-const shoppingCartTotal = document.querySelector(".shoppingCartTotal");
 
-function getCardItem() {
-  axios
-    .get(`${baseUrl}/api/livejs/v1/customer/${api_path}/carts`)
-    .then((res) => {
-      shoppingCartTable.innerHTML = "";
-      let string = `<caption></caption><tr>
-      <th width="40%">品項</th>
-      <th width="15%">單價</th>
-      <th width="15%">數量</th>
-      <th width="15%">金額</th>
-      <th width="15%"></th>
-    </tr>`
-      res.data.carts.forEach((item) => {
-        string += `
-        <td>
-            <div class="cardItem-title">
-              <img src="${item.product.images}" alt="" />
-              <p>${item.product.title}</p>
-            </div>
-          </td>
-          <td>NT$${item.product.price}</td>
-          <td>${item.quantity}</td>
-          <td>NT$${item.product.price * item.quantity}</td>
-          <td class="discardBtn">
-            <a href="#" class="material-icons"> clear </a>
-          </td>
-        `;
-      });
-
-      string + `<tr>
-      <td>
-        <a href="#" class="discardAllBtn">刪除所有品項</a>
-      </td>
-      <td></td>
-      <td></td>
-      <td>
-        <p>總金額</p>
-      </td>
-      <td class="shoppingCartTotal">NT$0</td>
-    </tr>`
-    shoppingCartTable.innerHTML += string
-      shoppingCartTotal.innerText = `NT$${res.data.finalTotal.toLocaleString()}`;
-      addToCartBtnListener();
-    })
-    .catch((err) => {
-      console.log(err);
-      alert("取得購物車列表失敗，請聯繫親切的工程師");
-    });
-}
-
-// `<td>
-//            <div class="cardItem-title">
-//              <img src="${item.product.images}" alt="" />
-//              <p>${item.product.title}</p>
-//            </div>
-//          </td>
-//          <td>NT$${item.product.price}</td>
-//          <td>${item.quantity}</td>
-//          <td>NT$${item.product.price * item.quantity}</td>
-//          <td class="discardBtn">
-//            <a href="#" class="material-icons"> clear </a>
-//          </td>`
-
-// 為按鈕加上監聽，取得購物車列表時就直接呼叫
+// 為「加入購物車」按鈕加上事件監聽，取得購物車列表時就直接呼叫
 function addToCartBtnListener() {
   const addToCartBtnArray = document.querySelectorAll(".addCardBtn");
   addToCartBtnArray.forEach(function (item) {
@@ -131,23 +65,78 @@ function addToCart(e) {
       alert("新增購物車失敗");
     });
 }
+
+// 取得購物車列表
+const cardItemWrapper = document.querySelector(".card-item-wrapper");
+const shoppingCartTable = document.querySelector(".shoppingCart-table");
+const shoppingCartTotal = document.querySelector(".shoppingCartTotal");
+
+function getCardItem() {
+  axios
+    .get(`${baseUrl}/api/livejs/v1/customer/${api_path}/carts`)
+    .then((res) => {
+      console.log(res.data);
+      shoppingCartTable.innerHTML = "";
+      let string = `<caption></caption><tr>
+      <th width="40%">品項</th>
+      <th width="15%">單價</th>
+      <th width="15%">數量</th>
+      <th width="15%">金額</th>
+      <th width="15%"></th>
+    </tr>`;
+      res.data.carts.forEach((item) => {
+        string += `
+        <tr>
+          <td>
+            <div class="cardItem-title">
+              <img src="${item.product.images}" alt="" />
+              <p>${item.product.title}</p>
+            </div>
+          </td>
+          <td>NT$${item.product.price.toLocaleString()}</td>
+          <td>${item.quantity}</td>
+          <td>NT$${(item.product.price * item.quantity).toLocaleString()}</td>
+          <td class="discardBtn">
+            <a href="#" class="material-icons"> clear </a>
+          </td>
+        </tr>
+        `;
+      });
+      string += `<tr>
+      <td>
+        <a href="#" class="discardAllBtn">刪除所有品項</a>
+      </td>
+      <td></td>
+      <td></td>
+      <td>
+        <p>總金額</p>
+      </td>
+      <td class="shoppingCartTotal">NT$${res.data.finalTotal.toLocaleString()}</td>
+    </tr>`;
+      // TODO: 寫一個判斷式，如果購物車是空的，就插入一段文字「購物車內沒有商品」
+      shoppingCartTable.innerHTML = string;
+      const discardAllBtn = document.querySelector(".discardAllBtn");
+      discardAllBtn.addEventListener("click", discardAll);
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("取得購物車列表失敗，請聯繫親切的工程師");
+    });
+}
 // 編輯購物車產品數量
 
 // 刪除所有購物車資料
-const discardAllBtn = document.querySelector(".discardAllBtn");
-discardAllBtn.addEventListener("click", discardAll);
-
 function discardAll(e) {
   e.preventDefault();
   axios
     .delete(`${baseUrl}/api/livejs/v1/customer/${api_path}/carts`)
     .then((res) => {
-      shoppingCartTotal.textContent = "NT$0";
-      cardItemWrapper.innerHTML = "購物車內沒有商品";
+      alert('所有品項刪除成功')
     })
     .catch((err) => {
       if (err.response.data.message) {
         alert("購物車已經是空的囉！");
+        getCardItem();
       } else {
         alert("刪除購物車項目失敗，請聯繫親切的工程師");
       }
